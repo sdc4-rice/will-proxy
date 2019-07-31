f538c6ae1e043c5daf604d4f22e6947705f8ed96
Will Resen <willresen@gmail.com>
2019-07-30 18:03:22 -0700

    Added proxy route for POST requests

--- server/index.js
+++ server/index.js
@@ -1,3 +1,5 @@
+require('newrelic');
+const fetch = require('node-fetch');
 const express = require('express');
 
 require('dotenv').config();
@@ -6,6 +8,17 @@ const app = express();
 const port = process.env.PORT;
 
 app.use(express.static('public'));
+app.use(express.json());
+
+app.all('/items', (req, res) => {
+  fetch('http://localhost:3001/items', {
+    method: req.method,
+    headers: req.headers,
+    body: JSON.stringify(req.body)
+  })
+    .then(results => results.json())
+    .then(results => res.send(results))
+})
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
